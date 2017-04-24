b79d7b600bb319c6e3d5d9d4a92468afdec9d23d
Jake Boxer <jake@github.com>
2017-04-23 20:52:59 -0700

    Get up to stackable

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+    int slotCount;
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    ItemDatabase itemDatabase;
+
+    public GameObject inventorySlot;
+    public GameObject inventoryItem;
+
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    void Start() {
+        slotCount = 16;
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+        itemDatabase = GetComponent<ItemDatabase>();
+
+        for (int i = 0; i < slotCount; i++) {
+            GameObject slot = Instantiate(inventorySlot);
+            slots.Add(slot);
+            slot.transform.SetParent(slotPanel.transform);
+
+            items.Add(new Item());
+        }
+
+        AddItem(0);
+        AddItem(1);
+        AddItem(1);
+        AddItem(1);
+        AddItem(1);
+    }
+
+    public void AddItem(int id) {
+        Item itemToAdd = itemDatabase.FindItemById(id);
+
+        if (itemToAdd.Stackable && IsInInventory(itemToAdd)) {
+            for (int i = 0; i < items.Count; i++) {
+                Item item = items[i];
+
+                if (item.ID == id) {
+                    GameObject itemGameObject = slots[i].transform.GetChild(0).gameObject;
+                    ItemData itemData = itemGameObject.GetComponent<ItemData>();
+
+                    itemData.amount++;
+                    itemGameObject.transform.FindChild("Stack Amount").GetComponent<Text>().text = itemData.amount.ToString();
+
+                    break;
+                }
+            }
+        } else {
+            for (int i = 0; i < items.Count; i++) {
+                if (items[i].ID == -1) {
+                    items[i] = itemToAdd;
+
+                    GameObject itemGameObject = Instantiate(inventoryItem);
+                    itemGameObject.transform.SetParent(slots[i].transform);
+                    itemGameObject.transform.position = Vector2.zero;
+                    itemGameObject.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                    itemGameObject.name = itemToAdd.Title;
+
+                    itemGameObject.GetComponent<ItemData>().amount = 1;
+
+                    break;
+                }
+            }
+        }
+    }
+
+    bool IsInInventory(Item itemToCheck) {
+        foreach (Item item in items) {
+            if (itemToCheck.ID == item.ID) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+
+public class ItemDatabase : MonoBehaviour {
+    private List<Item> items = new List<Item>();
+    private JsonData itemData;
+
+    void Start() {
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+        SetUpItems();
+    }
+
+    void SetUpItems() {
+        foreach (JsonData elem in itemData) {
+            items.Add(new Item(elem));
+        }
+    }
+
+    public Item FindItemById(int id) {
+        foreach (Item item in items) {
+            if (item.ID == id) {
+                return item;
+            }
+        }
+
+        return null;
+    }
+}
+
+public class Item {
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public int Value { get; set; }
+    public int Power { get; set; }
+    public int Defense { get; set; }
+    public int Vitality { get; set; }
+    public string Description { get; set; }
+    public bool Stackable { get; set; }
+    public int Rarity { get; set; }
+    public string Slug { get; set; }
+    public Sprite Sprite { get; set; }
+
+    public Item() {
+        ID = -1;
+    }
+
+    public Item(JsonData data) {
+        ID = (int)data["id"];
+        Title = data["title"].ToString();
+        Value = (int)data["value"];
+        Power = (int)data["stats"]["power"];
+        Defense = (int)data["stats"]["defense"];
+        Vitality = (int)data["stats"]["vitality"];
+        Description = data["description"].ToString();
+        Stackable = (bool)data["stackable"];
+        Rarity = (int)data["rarity"];
+        Slug = data["slug"].ToString();
+
+        Sprite = Resources.Load<Sprite>("Sprites/Items/" + Slug);
+    }
+}
